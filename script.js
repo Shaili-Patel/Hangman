5e8781d9c0c18fb229506c87c8ca6c899842efa5
Shaili-Patel <526shailipatel@frhsd.com>
2025-03-24 11:50:29 -0400

    Background

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const wordList = [
   "Neo",
-  "luck",
   "DejaVu",
-  "rain",
-  "Dream",
+  "TheOne",
   "RedPill",
   "BluePill",
   "Illusion",
-  "celebration",
+  "Simulation",
   "Morpheus",
   "TheKeymaker",
-  "tradition",
+  "The Oracle",
 ];
 
 // Setting Game Variables
-let selectWord = "";
+let selectedWord = "";
 let displayWord = "";
 let wrongGuess = 0;
 let guessedLetters = [];
 const maxMistakes = 6;
 
 function startGame(level) {
-  selectWord = getRandomWord(level);
+  selectedWord = getRandomWord(level);
 
 
   // Hide Difficulty Selection and Show Game Area & Difficulty Box
@@ -46,4 +44,48 @@ function getRandomWord(level) {
   });
 
   return filteredWords[Math.floor(Math.random() * filteredWords.length)];
+}
+
+function updateDifficultyDisplay(level){
+  let difficultyBox = document.getElementById('difficultyBox')
+
+  // remove any pevious difficulty classes 
+  difficultyBox.classList.remove('easy', 'medium', 'hard')
+
+  // set text & apply class dynamically using template literals
+  difficultyBox.textContent = `Difficulty: ${level.charAt(0).toUpperCase() + level.slice(1)}`
+
+  // only the appropriate CSS style for chosen difficulty
+  difficultyBox.classList.add(level) 
+}
+
+function guessLetter(){
+  let inputField = document.getElementById('letterInput') //Get input field
+  let guessedLetter = inputField.value.toLowerCase() //Convert input to Lowercase
+
+// Check if input is a valid letter (a-z)
+if(!guessedLetter.match(/^[a-z]$/)){
+alert()
+inputField.value = '' //Clear input field
+return //Exit function
+
+}
+
+// Check if letter was already guessed using .includes()
+if(guessedLetters.includes(guessedLetter)){
+  alert(`You already guessed '${guessedLetter}'. Try a different letter!`)
+  inputField.value = '' //Clear input field
+  return //Exit function
+} else {
+  guessedLetters.push(guessedLetter)
+}
+
+if(selectedWord.includes(guessedLetters)){
+  correctGuess(guessedLetter)
+}else{
+  wrongGuesses(guessedLetter)
+}
+
+inputField.value='' //Clear input field
+inputField.focus() //Refocus input field for the next guess
 }
